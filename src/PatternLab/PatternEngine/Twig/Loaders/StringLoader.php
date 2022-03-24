c486e578482e94d4a5dea6c9082bae4e03ebe96b
Shaun Drong <sdrong@uceap.universityofcalifornia.edu>
2022-03-24 16:21:30 -0700

    even more twig class call updates

--- src/PatternLab/PatternEngine/Twig/Loaders/StringLoader.php
+++ src/PatternLab/PatternEngine/Twig/Loaders/StringLoader.php
@@ -16,44 +16,48 @@ use \PatternLab\Config;
 use \PatternLab\Dispatcher;
 use \PatternLab\PatternEngine\Loader;
 use \PatternLab\PatternEngine\Twig\TwigUtil;
+use Twig\Loader\FilesystemLoader as Twig_Loader_Filesystem;
+use Twig\Environment as Twig_Environment;
+use Twig\Loader\ChainLoader as Twig_Loader_Chain;
+use Twig\Loader\ArrayLoader;
 
 class StringLoader extends Loader {
-	
+
 	/**
 	* Load a new Twig instance that is just a vanilla Twig rendering engine for strings
 	*/
 	public function __construct($options = array()) {
-		
+
 		// set-up the defaults
 		$twigDebug = Config::getOption("twigDebug");
-		
+
 		// go through various places where things can exist
 		$filesystemLoaderPaths = array();
-		
+
 		// see if source/_macros exists
 		$macrosPath = Config::getOption("sourceDir").DIRECTORY_SEPARATOR."_macros";
 		if (is_dir($macrosPath)) {
 			$filesystemLoaderPaths[] = $macrosPath;
 		}
-		
+
 		// see if source/_layouts exists. if so add it to be searchable
 		$layoutsPath = Config::getOption("sourceDir").DIRECTORY_SEPARATOR."_layouts";
 		if (is_dir($layoutsPath)) {
 			$filesystemLoaderPaths[] = $layoutsPath;
 		}
-		
+
 		// set-up the loader list
 		$loaders = array();
 		// add the paths to the filesystem loader if the paths existed
 		if (count($filesystemLoaderPaths) > 0) {
-			$loaders[] = new \Twig_Loader_Filesystem($filesystemLoaderPaths);
+			$loaders[] = new Twig_Loader_Filesystem($filesystemLoaderPaths);
 		}
-		$loaders[] = new \Twig_Loader_String();
-		
+		$loaders[] = new ArrayLoader();
+
 		// set-up Twig
-		$twigLoader = new \Twig_Loader_Chain($loaders);
-		$instance   = new \Twig_Environment($twigLoader, array("debug" => $twigDebug));
-		
+		$twigLoader = new Twig_Loader_Chain($loaders);
+		$instance   = new Twig_Environment($twigLoader, array("debug" => $twigDebug));
+
 		// customize Twig
 		TwigUtil::setInstance($instance);
 		// Disabling custom Twig Extensions for String loader as it is only used internally by PL
@@ -66,7 +70,7 @@ class StringLoader extends Loader {
 		TwigUtil::loadDateFormats();
 		TwigUtil::loadDebug();
 		TwigUtil::loadMacros();
-		
+
 		// set-up the dispatcher
 		$dispatcherInstance = Dispatcher::getInstance();
 		$dispatcherInstance->dispatch("twigLoader.customize");
@@ -74,9 +78,9 @@ class StringLoader extends Loader {
 
 		// get the instance
 		$this->instance = TwigUtil::getInstance();
-		
+
 	}
-	
+
 	/**
 	* Render a string
 	* @param  {Array}        the options to be rendered by Twig
@@ -84,9 +88,9 @@ class StringLoader extends Loader {
 	* @return {String}       the rendered result
 	*/
 	public function render($options = array()) {
-		
+
 		return $this->instance->render($options["string"], $options["data"]);
-		
+
 	}
-	
+
 }
